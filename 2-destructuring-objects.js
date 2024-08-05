f7ce30d6ce58409013ef92f621fc671f2f317804
Begüm Başovalı <begumbasovali@posta.mu.edu.tr>
2024-08-05 14:21:04 +0300

    example for destructuring objects

--- 2-destructuring-objects.js
+++ 2-destructuring-objects.js
@@ -14,3 +14,19 @@ const { name, club, address: {city} } = player;
 //console.log(`${name} plays for ${club}`);
 console.log(`${name} lives in ${city}`);
 
+
+//Exercise 
+const student = {
+  name: "Kyle",
+  age: 24,
+  projects: {
+      diceGame: "Two player dice game using JavaScript"
+  }
+}
+//destructure the object in above
+const { name, age, projects: { diceGame } } = student;
+
+console.log(name);
+console.log(age);
+console.log(diceGame);
+
