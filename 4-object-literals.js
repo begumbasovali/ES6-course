45d39ec211f4018fb82b4352448b469f123b8783
Begüm Başovalı <begumbasovali@posta.mu.edu.tr>
2024-08-05 14:36:34 +0300

    challenge

--- 4-object-literals.js
+++ 4-object-literals.js
@@ -6,3 +6,32 @@ function addressMaker(city, state) {
 }
 
 addressMaker('Begum', 'LA');
+
+
+//Challenge: Use destructuring for the code below
+
+
+/*function addressMaker(address) {
+  const newAddress = {
+      city: address.city,
+      state: address.state,
+      country: 'United States'
+  };
+  
+}
+
+addressMaker({city: 'Austin', state: 'Texas'});
+*/
+
+function addressMaker(address) {
+  const {city, state} = adress;
+  const newAddress = {
+      city,
+      state,
+      country: 'United States'
+  };
+  console.log(`${newAddress.city}, ${newAddress.state},${newAddress.country}`)
+  
+}
+
+addressMaker({city: 'Begum', state: 'NYC'});
